get_noise_cov_pixel : Fixing bug where only one noise realisation **2 was saved as noise covariance insteand of noise_cov_mean ... simulations250 / test_preproc_mpi250 : yml files to run on 250 simulations and also includes new mask_handler_binary_zero_threshold parameter in masks subsection. test_mpi_noise_cov.sh : used the 250 version of the yml files noise_cov_testing.ipynb : uses updated covariances added case 7b and 7c to compare results of 7a with perfect white noise cas (7b) and a more restrictive sky patch (7c)

pipeline/get_noise_cov_pixel.py
@@ -306,9 +306,9 @@ def GetNoiseCov(args, apply_pixwin=False):
             # Saving noise_cov and noise_cov_preprocessed to disk
             print('SAVING NOISE COV AND NOISE COV PREPROCESSED TO DISK')
             np.save(os.path.join(meta.covmat_directory, 'pixel_noise_cov.npy' ),
-                    noise_cov )
+                    noise_cov_mean )
             np.save(os.path.join(meta.covmat_directory, 'pixel_noise_cov_preprocessed.npy' ),
-                    noise_cov_preprocessed )  
+                    noise_cov_preprocessed_mean )  
 
             
             current, peak = tracemalloc.get_traced_memory()