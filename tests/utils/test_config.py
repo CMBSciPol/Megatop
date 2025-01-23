79353f105f204284541e604a8d9da21ff59b5364
Simon Biquard <sbiquard+git@gmail.com>
2025-01-24 08:09:19 +0900

    Fix failing test

--- tests/utils/test_config.py
+++ tests/utils/test_config.py
@@ -4,5 +4,5 @@ from megatop.utils.config import _GeneralPars
 
 
 def test_lmax_validator():
-    with pytest.raises(ValueError, match="should be lower or equal to"):
+    with pytest.raises(ValueError, match="less than or equal to"):
         _GeneralPars(nside=128, lmax=500)
