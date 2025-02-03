3c31e1c30f504a8e584f5ba728755f82e642cd5f
BaptisteJost <jost.baptiste@gmail.com>
2025-02-03 17:47:24 +0900

    adding comp_sep_plotter in plot_

---
+++ src/megatop/plot_/comp_sep_plotter.py
@@ -0,0 +1,65 @@
+import argparse
+from pathlib import Path
+
+import numpy as np
+
+from megatop import DataManager
+from megatop.config import Config
+from megatop.utils import Timer, logger
+from megatop.utils.plot import freq_maps_plotter
+from megatop.utils.preproc import _apply_binary_mask
+
+
+def plot_compsep(manager, config):
+    plot_dir = manager.path_to_components_plots
+    plot_dir.mkdir(parents=True, exist_ok=True)
+
+    comp_maps = np.load(manager.path_to_components_maps)
+    comp_maps = _apply_binary_mask(manager, comp_maps, unseen=True)
+
+    freq_maps_plotter(
+        config,
+        np.array([comp_maps[0]]),
+        plot_dir,
+        "CMB_post_compsep_maps",
+        component="CMB post-compsep",
+    )
+    freq_maps_plotter(
+        config,
+        np.array([comp_maps[1]]),
+        plot_dir,
+        "dust_post_compsep_maps",
+        component="Dust post-compsep",
+    )
+    freq_maps_plotter(
+        config,
+        np.array([comp_maps[2]]),
+        plot_dir,
+        "synch_post_compsep_maps",
+        component="Synch post-compsep",
+    )
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Plotter for component separation output")
+    parser.add_argument("--config", type=Path, help="config file")
+    args = parser.parse_args()
+    if args.config is None:
+        logger.warning("No config file provided, using example config")
+        config = Config.get_example()
+    else:
+        config = Config.from_yaml(args.config)
+    manager = DataManager(config)
+    manager.dump_config()
+
+    logger.info("Plotting comp sep outputs...")
+    timer = Timer()
+    timer.start("compsep_plotter")
+
+    plot_compsep(manager, config)
+
+    timer.stop("compsep_plotter", "Plotting comp sep outputs")
+
+
+if __name__ == "__main__":
+    main()
