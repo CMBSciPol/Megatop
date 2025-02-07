f74b9c789839e55358ceb06d49416967b44043c9
Benjamin Beringue <beringueb@gmail.com>
2025-02-07 17:45:06 +0900

    Cleaning utils.

--- src/megatop/utils/__init__.py
+++ src/megatop/utils/__init__.py
@@ -1,9 +1,7 @@
 from .logger import logger
-from .metadata_manager import BBmeta
 from .timer import Timer, function_timer
 
 __all__ = [
-    "BBmeta",
     "Timer",
     "function_timer",
     "logger",
