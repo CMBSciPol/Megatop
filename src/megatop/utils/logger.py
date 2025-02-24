54b1832a1ae33f721e9a844aa595e25b54c5cb6a
Simon Biquard <sbiquard+git@gmail.com>
2025-02-24 16:24:19 +0100

    make logger MPI-aware
    - if world size is larger than 1, commit msg includes process rank
    - remove module and lineno

--- src/megatop/utils/logger.py
+++ src/megatop/utils/logger.py
@@ -1,14 +1,38 @@
 import logging
 import os
 
+from megatop.utils.mpi import get_world
+
 __all__ = [
     "logger",
 ]
 
+try:
+    # Get MPI information
+    comm, rank, size = get_world()
+except ImportError:
+    rank = 0
+    size = 1
+
+log_format = (
+    "%(asctime)s - Rank %(rank)d - %(levelname)s: %(message)s"
+    if size > 1
+    else "%(asctime)s - %(levelname)s: %(message)s"
+)
 
 logging.basicConfig(
-    format="%(asctime)s - %(module)s:%(lineno)d - %(levelname)s: %(message)s",
+    format=log_format,
     datefmt="%d-%b-%y %H:%M:%S",
 )
+
+
+# Custom filter to include rank in log messages
+class RankFilter(logging.Filter):
+    def filter(self, record):
+        record.rank = rank
+        return True
+
+
 logger = logging.getLogger("megatop")
+logger.addFilter(RankFilter())
 logger.setLevel(os.getenv("LOGLEVEL", "INFO").upper())
