3f5a8f8dfeb7006702cf3cb046685107e809eb3e
BaptisteJost <jost.baptiste@gmail.com>
2025-08-07 18:21:46 +0200

    added plot_all_spectra() to show spectra from all sky simulations update to common binning

--- src/megatop/plot/noise_spectra_plotter.py
+++ src/megatop/plot/noise_spectra_plotter.py
@@ -1,25 +1,106 @@
 import argparse
 from pathlib import Path
 
+import matplotlib.pyplot as plt
 import numpy as np
-import pymaster as nmt
 
 from megatop import Config, DataManager
 from megatop.utils import Timer, logger
+from megatop.utils.binning import load_nmt_binning
 from megatop.utils.mock import get_Cl_CMB_model_from_manager
 from megatop.utils.plot import plot_all_Cls, plot_all_Cls_diff
+
+
+def plot_all_spectra(manager, config):
+    plot_dir = manager.path_to_spectra_plots
+    plot_dir.mkdir(parents=True, exist_ok=True)
+
+    binning_info = np.load(manager.path_to_binning, allow_pickle=True)
+    bin_centre_lminlmax = binning_info["bin_centre_lminlmax"]
+    bin_index_lminlmax = binning_info["bin_index_lminlmax"]
+
+    Cl_cmb_model = get_Cl_CMB_model_from_manager(manager)[0, :, : 3 * config.nside]
+    # nmt_bins = nmt.NmtBin.from_edges(binning_info["bin_low"], binning_info["bin_high"] + 1)
+    nmt_bins = load_nmt_binning(manager)
+
+    bined_Cl_cmb_model = nmt_bins.bin_cell(Cl_cmb_model)[:, bin_index_lminlmax]
+
+    fig_EE, ax_EE = plt.subplots()
+    fig_BB, ax_BB = plt.subplots()
+
+    average_noise_CMB = np.zeros([4, len(bin_centre_lminlmax)])
+    for id_sim in range(config.map_sim_pars.n_sim):
+        fname_noise_Cls = manager.get_path_to_noise_spectra_cross_components(sub=id_sim)
+        all_noise_Cls = np.load(fname_noise_Cls, allow_pickle=True)
+
+        fname_all_Cls = manager.get_path_to_spectra_cross_components(sub=id_sim)
+        all_Cls = np.load(fname_all_Cls, allow_pickle=True)
+
+        debiased_cmb_cls = all_Cls["CMBxCMB"]  # - all_noise_Cls["Noise_CMBxNoise_CMB"]
+        average_noise_CMB += all_noise_Cls["Noise_CMBxNoise_CMB"]
+
+        # negative_bins_in_EE = np.any(debiased_cmb_cls[0] < 0)
+        # negative_bins_in_BB = np.any(debiased_cmb_cls[-1] < 0)
+        # if negative_bins_in_BB or negative_bins_in_EE:
+        #     logger.warning(f"Negative bins in CMB spectra for simulation {id_sim}. In EE: {negative_bins_in_EE}, In BB: {negative_bins_in_BB}")
+        ax_EE.plot(
+            bin_centre_lminlmax,
+            debiased_cmb_cls[0],
+            label="Estimated CMB EE (noisy)" if id_sim == 0 else None,
+            linestyle="-",
+            color="darkblue",
+            alpha=0.2,  # if not negative_bins_in_EE else 1.0,
+        )
+        ax_BB.plot(
+            bin_centre_lminlmax,
+            debiased_cmb_cls[-1],
+            label="Estimated CMB BB (noisy)" if id_sim == 0 else None,
+            linestyle="-",
+            color="darkblue",
+            alpha=0.2,  # if not negative_bins_in_BB else 1.0,
+        )
+
+    average_noise_CMB /= config.map_sim_pars.n_sim
+
+    ax_EE.plot(
+        bin_centre_lminlmax,
+        bined_Cl_cmb_model[1] + average_noise_CMB[0],
+        label="CMB EE model + mean noise",
+        color="black",
+        linestyle="--",
+    )
+    ax_BB.plot(
+        bin_centre_lminlmax,
+        bined_Cl_cmb_model[2] + average_noise_CMB[-1],
+        label="CMB BB model + mean noise",
+        color="black",
+        linestyle="--",
+    )
+
+    ax_EE.set_xlabel(r"$\ell$")
+    ax_EE.set_ylabel(r"$C_{\ell}^{EE}$")
+    ax_EE.legend()
+    ax_EE.loglog()
+    ax_EE.set_title("CMB EE spectra")
+    fig_EE.savefig(plot_dir / "allskysims_CMB_EE_spectra.png")
+
+    ax_BB.set_xlabel(r"$\ell$")
+    ax_BB.set_ylabel(r"$C_{\ell}^{BB}$")
+    ax_BB.legend()
+    ax_BB.loglog()
+    ax_BB.set_title("CMB BB spectra")
+    fig_BB.savefig(plot_dir / "allskysims_CMB_BB_spectra.png")
+    # closing figures
+    plt.close(fig_EE)
+    plt.close(fig_BB)
 
 
 def plot_noise_spectra(manager, config, id_sim=None):
     plot_dir = manager.path_to_spectra_plots
     plot_dir.mkdir(parents=True, exist_ok=True)
 
-    # path_binning = manager.get_path_to_spectra_binning(sub=id_sim)
-    # binning_info = np.load(path_binning, allow_pickle=True)
-    # bin_centre_lminlmax = binning_info["bin_centre_lminlmax"]
     binning_info = np.load(manager.path_to_binning, allow_pickle=True)
-    bin_centre_lminlmax = binning_info['bin_centre_lminlmax']
-
+    bin_centre_lminlmax = binning_info["bin_centre_lminlmax"]
 
     fname_noise_Cls = manager.get_path_to_noise_spectra_cross_components(sub=id_sim)
     all_noise_Cls = np.load(fname_noise_Cls, allow_pickle=True)
@@ -50,7 +131,7 @@ def plot_noise_spectra(manager, config, id_sim=None):
     )
 
     Cl_cmb_model = get_Cl_CMB_model_from_manager(manager)[0, :, : 3 * config.nside]
-    nmt_bins = nmt.NmtBin.from_edges(binning_info["bin_low"], binning_info["bin_high"] + 1)
+    nmt_bins = load_nmt_binning(manager)
 
     bined_Cl_cmb_model = nmt_bins.bin_cell(Cl_cmb_model)[:, binning_info["bin_index_lminlmax"]]
 
@@ -114,6 +195,10 @@ def main():
         id_sim = 0
 
     plot_noise_spectra(manager, config, id_sim=id_sim)
+
+    if n_sim_sky != 0:
+        logger.info("Plotting all spectra:")
+        plot_all_spectra(manager, config)
 
     timer.stop("Noise_spectra_plotter")
 
