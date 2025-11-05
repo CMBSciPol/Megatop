cdc41c0f6c86e1d25ad147928fc690610eea4706
Simon Biquard <sbiquard+git@gmail.com>
2025-11-05 16:26:43 +0000

    improve get_example_config.py script

--- get_example_config.py
+++ get_example_config.py
@@ -1,11 +1,9 @@
+#!/usr/bin/env python3
+
 from pathlib import Path
 
-import megatop
 from megatop import Config, DataManager
 
 default_config = Config.get_example()
-
-megatop_path = Path(megatop.__path__[0]).parents[1]
-paramfiles_path = megatop_path / "paramfiles"
 manager = DataManager(default_config)
-manager.dump_config(paramfiles_path / "default_config.yaml")
+manager.dump_config(Path(__file__).parent / "paramfiles/default_config.yaml")
