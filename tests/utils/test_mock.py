fix: off-by-one error in test

tests/utils/test_mock.py
@@ -22,8 +22,9 @@ def test_shape_white_noise_map():
 
 
 def test_shape_spectra_noise_map():
-    nell = np.arange(2, 2 * NSIDE)
-    freq_maps = mock.get_noise_map_from_noise_spectra(nell, NSIDE, 2 * NSIDE)
+    lmax = 2 * NSIDE
+    nell = np.arange(2, lmax + 1)
+    freq_maps = mock.get_noise_map_from_noise_spectra(nell, NSIDE, lmax)
     assert freq_maps.shape == (3, hp.nside2npix(NSIDE))
 
 