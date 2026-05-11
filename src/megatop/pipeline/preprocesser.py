16cc08f18e05d61f170c8261dacd6504a4113abe
Simon Biquard <79218427+sbiquard@users.noreply.github.com>
2026-05-11 16:15:12 +0100

    remove outdate comment

--- src/megatop/pipeline/preprocesser.py
+++ src/megatop/pipeline/preprocesser.py
@@ -41,7 +41,6 @@ def preprocess_map(
 
     if beams_match and not use_harmonic:
         logger.info("Common beam correction is the same as the input beam, no need to apply it.")
-        logger.warning("This is mostly for testing it might not actually represent the real noise")
         freq_maps_convolved = np.array(input_maps, dtype="float64")
     else:
         freq_maps_convolved = common_beam_and_nside(
