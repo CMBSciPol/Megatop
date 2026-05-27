6a0e8232e9b709bd404f9260de7bfceed91b66a6
Malcolm Landais <landais@mac.home>
2026-05-27 23:37:04 +0200

    Bar HMC working end to end Birefringence pipeline

--- Megatop/runfiles/run_default.sh
+++ Megatop/runfiles/run_default.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+export FI_PROVIDER=tcp
 PARAM_FILE="../paramfiles/default_config.yaml"
 
 echo "Running pipeline with paramfile: ${PARAM_FILE}"
@@ -12,56 +12,58 @@ conda activate megatop
 echo "------------------------------------------------------------"
 echo "|                        MASK-HANDLER                      |"
 echo "------------------------------------------------------------"
-megatop-mask-run --config ${PARAM_FILE}
+#megatop-mask-run --config ${PARAM_FILE}
 echo ""
 echo ""
 echo "Plotting mask outputs"
-megatop-mask-plot --config ${PARAM_FILE}
+#megatop-mask-plot --config ${PARAM_FILE}
 
 echo "------------------------------------------------------------"
 echo "|                       BINNING-MAKER                      |"
 echo "------------------------------------------------------------"
-megatop-binning-run --config ${PARAM_FILE}
+#megatop-binning-run --config ${PARAM_FILE}
 echo ""
 echo ""
 
 echo "------------------------------------------------------------"
 echo "|                           MOCKER                         |"
 echo "------------------------------------------------------------"
-megatop-mock-run --config ${PARAM_FILE}
+#megatop-mock-run --config ${PARAM_FILE}
 echo ""
 echo ""
 echo "Plotting mocker outputs"
-megatop-mock-plot --config ${PARAM_FILE}
+#megatop-mock-plot --config ${PARAM_FILE}
 
 echo "------------------------------------------------------------"
 echo "|            TRANSFER FUNCTION COMPUTATION                  |"
 echo "------------------------------------------------------------"
-megatop-TFcomputing-run --config ${PARAM_FILE}
+#megatop-TFcomputing-run --config ${PARAM_FILE}
 echo ""
 echo ""
 
 echo "------------------------------------------------------------"
 echo "|                       PRE-PROCESSER                      |"
 echo "------------------------------------------------------------"
-megatop-preproc-run --config ${PARAM_FILE}
+#megatop-preproc-run --config ${PARAM_FILE}
 echo ""
 echo ""
 echo "Plotting pre-processer outputs"
-megatop-preproc-plot --config ${PARAM_FILE}
+#megatop-preproc-plot --config ${PARAM_FILE}
 
 echo "------------------------------------------------------------"
 echo "|                NOISE-COVARIANCE COMPUTATION              |"
 echo "------------------------------------------------------------"
-megatop-noisecov-run --config ${PARAM_FILE}
+#FI_PROVIDER=tcp mpirun -n 10 python $(which megatop-noisecov-run) --config ${PARAM_FILE}
+#megatop-noisecov-run --config ${PARAM_FILE}
 echo ""
 echo ""
 echo "Plotting noise covariance outputs"
-megatop-noisecov-plot --config ${PARAM_FILE}
+#megatop-noisecov-plot --config ${PARAM_FILE}
 
 echo "------------------------------------------------------------"
 echo "|                    COMPONENT SEPARATION                  |"
 echo "------------------------------------------------------------"
+#FI_PROVIDER=tcp mpirun -n 10 python $(which megatop-compsep-run) --config ${PARAM_FILE}
 megatop-compsep-run --config ${PARAM_FILE}
 echo ""
 echo ""
@@ -71,24 +73,27 @@ megatop-compsep-plot --config ${PARAM_FILE}
 echo "------------------------------------------------------------"
 echo "|                     SPECTRA ESTIMATION                   |"
 echo "------------------------------------------------------------"
-megatop-map2cl-run --config ${PARAM_FILE}
+#FI_PROVIDER=tcp mpirun -n 10 python $(which megatop-map2cl-run) --config ${PARAM_FILE}
+#megatop-map2cl-run --config ${PARAM_FILE}
 echo ""
 echo ""
 echo "Plotting spectra estimater outputs"
-megatop-map2cl-plot --config ${PARAM_FILE}
+#megatop-map2cl-plot --config ${PARAM_FILE}
 
 echo "------------------------------------------------------------"
 echo "|                  NOISE SPECTRA ESTIMATION                |"
 echo "------------------------------------------------------------"
-megatop-noisespectra-run --config ${PARAM_FILE}
+#FI_PROVIDER=tcp mpirun -n 10 python $(which megatop-noisespectra-run) --config ${PARAM_FILE}
+#megatop-noisespectra-run --config ${PARAM_FILE}
 echo ""
 echo ""
 echo "Plotting noise spectra estimater outputs"
-megatop-noisespectra-plot --config ${PARAM_FILE}
+#megatop-noisespectra-plot --config ${PARAM_FILE}
 
 echo "------------------------------------------------------------"
 echo "|            COSMOLOGICAL PARAMETERS ESTIMATION            |"
 echo "------------------------------------------------------------"
+#FI_PROVIDER=tcp mpirun -n 10 python $(which megatop-cl2r-run) --config ${PARAM_FILE}
 megatop-cl2r-run --config ${PARAM_FILE}
 echo ""
 echo ""
