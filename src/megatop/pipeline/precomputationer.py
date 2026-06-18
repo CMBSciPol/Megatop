6f84ae8060408c6bbd16a495e877cd9a3a77e10f
BaptisteJost <jost.baptiste@gmail.com>
2026-06-18 05:46:41 +0100

    using logger instead of print

--- src/megatop/pipeline/precomputationer.py
+++ src/megatop/pipeline/precomputationer.py
@@ -2,6 +2,7 @@ import argparse
 from pathlib import Path
 
 from megatop import Config, DataManager
+from megatop.utils import logger
 from megatop.utils.mpi import get_world
 from megatop.utils.precomputations import megabuster_precomputations
 
@@ -15,14 +16,14 @@ def main():
     manager = DataManager(config)
 
     world, rank, size = get_world()
-    print(f"Rank {rank} of {size} is running")
+    logger.info(f"Rank {rank} of {size} is running")
     if rank == 0:
         manager.dump_config()
 
     if config.parametric_sep_pars.use_megabuster:
         megabuster_precomputations(manager=manager, config=config)
     else:
-        print("Skipping Megabuster precomputations as use_megabuster is set to False.")
+        logger.info("Skipping Megabuster precomputations as use_megabuster is set to False.")
 
 
 if __name__ == "__main__":
